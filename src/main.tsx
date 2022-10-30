9555eff504513e1dfeea8df6287ad1bef287f83a
José Moreira <josemoreiravarzim@gmail.com>
2022-10-30 17:18:53 +0000

    chore: add readme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
+import { convertToBase64 } from './data'
 import './index.css'
+import { data } from './mock'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+console.log(convertToBase64(data))
